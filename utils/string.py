0969510f35771980bcf44a26dae9747ac59ef7e9
Otávio Brito <o.augusto.brito@gmail.com>
2024-03-26 18:17:43 -0300

    Fist Commit

---
+++ utils/string.py
@@ -0,0 +1,10 @@
+import hmac
+
+def safe_str_cmp(a: str, b: str) -> bool:
+    if isinstance(a, str):
+        a = a.encode("utf-8")  # type: ignore
+
+    if isinstance(b, str):
+        b = b.encode("utf-8")  # type: ignore
+
+    return hmac.compare_digest(a, b)
